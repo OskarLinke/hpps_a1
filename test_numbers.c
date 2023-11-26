ee16f118357bba95490ceac9f5ec6c5e22ca52c5
OskarLinke <ingenandre@OskarsPC>
2023-11-26 13:22:23 +0100

    Implementerede bits8_to_int() og bits8_print()

--- test_numbers.c
+++ test_numbers.c
@@ -2,5 +2,12 @@
 
 //Inkluderer det bibliotek vi benytter
 int main() {
-
+    struct bits8 bit; 
+    bit.b0.v = false; 
+    bit.b1.v = true; 
+    bit.b7.v = true; 
+    bit.b5.v = true;
+    bits8_print(bit); 
+    printf("%d \n", bits8_to_int(bit)); 
+    
 }
