262a93847c0e6b275fcf3e60b3c563efe8b45f0c
wylich <frederik.wylich@gmail.com>
2023-11-26 14:11:33 +0100

    updated bits8_from_int

--- test_numbers.c
+++ test_numbers.c
@@ -8,6 +8,8 @@ int main() {
     bit.b7.v = true; 
     bit.b5.v = true;
     bits8_print(bit); 
-    printf("%d \n", bits8_to_int(bit)); 
+    printf("%d \n", bits8_to_int(bit));
+    unsigned x = 100;
+    bits8_to_int(bits8_from_int(x)) == x;
     
 }
