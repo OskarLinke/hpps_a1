b3dfa7dcf579f98c5a47a8c56a9e9f52013a1392
wylich <frederik.wylich@gmail.com>
2023-11-26 13:44:22 +0100

    bits8_from_int implemented

--- numbers.h
+++ numbers.h
@@ -14,5 +14,22 @@ struct bits8{
     struct bit b7;
 }; 
 
-
-
+struct bits8 bits8_from_int(unsigned int x) {
+    struct bits8 bits;
+    bits.b7 = bit_from_int(x / 128);
+    x = x % 128;
+    bits.b6 = bit_from_int(x / 64);
+    x = x % 64;
+    bits.b5 = bit_from_int(x / 32);
+    x = x % 32;
+    bits.b4 = bit_from_int(x / 16);
+    x = x % 16;
+    bits.b3 = bit_from_int(x / 8);
+    x = x % 8;
+    bits.b2 = bit_from_int(x / 4);
+    x = x % 4;
+    bits.b1 = bit_from_int(x / 2);
+    x = x % 2;
+    bits.b0 = bit_from_int(x / 1);
+    return bits;
+}
